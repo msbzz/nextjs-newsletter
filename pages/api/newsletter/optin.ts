e2e15444eb1e61b38eab17c3a245e19bc1a9daee
Marco <mbarozzi@gmail.com>
2024-11-06 05:47:18 -0300

    cadastrando na tabela do supabase

--- pages/api/newsletter/optin.ts
+++ pages/api/newsletter/optin.ts
@@ -1,75 +1,76 @@
-import {createClient} from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
 import { NextApiRequest, NextApiResponse } from "next";
 
 // Supabase Setup
 //===============
 //const SUPABASE_KEY = process.env.SUPABASE_KEY;
-const SUPABASE_KEY= process.env.SUPABASE_SERVICE_KEY;
+const SUPABASE_KEY = process.env.SUPABASE_SERVICE_KEY;
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const dbClient = createClient(SUPABASE_URL,SUPABASE_KEY);
+const dbClient = createClient(SUPABASE_URL, SUPABASE_KEY);
 //===============
 
-const httpStatus ={
-  Success:200,
-  BadRequest:400,
-  NotFound:404,
-  InternalServerError:500
-}
+const httpStatus = {
+  Success: 200,
+  BadRequest: 400,
+  NotFound: 404,
+  InternalServerError: 500,
+};
 
-const controllerByMethod ={
-  async POST(req:NextApiRequest,res:NextApiResponse){
-    console.log(req.body.emailNewsletter)
-    
-    const email = req.body.emailNewsletter
-    
+const controllerByMethod = {
+  async POST(req: NextApiRequest, res: NextApiResponse) {
+    console.log(req.body.emailNewsletter);
+
+    const email = req.body.emailNewsletter;
+
     // fail fast validation
-    if(!Boolean(email) || !email.includes('@')){
-
-      //console.log('dentro da critica')
-
+    if (!Boolean(email) || !email.includes("@")) {
       res
-      .status(httpStatus.BadRequest)
-      .json({message:"voce precisa enviar um email valido ex:'teste@email.com"});
+        .status(httpStatus.BadRequest)
+        .json({
+          message: "voce precisa enviar um email valido ex:'teste@email.com",
+        });
       return;
     }
 
-    res
-    .status(httpStatus.Success)
-    .json({message:'Post request'})
+    // sanitize email
+    // remove malicious code
+    // remove x things
+
+    // adiciona pessoa na newsletter
+    const{ error } =await dbClient.from("newsletter_users").insert({email:email,optin:true});
+     // if(error){}  desenvolver critica de erro
+     // Cria usuario de fato no sistema
+     await dbClient.auth.admin.createUser({email:email}) 
+    
+    res.status(httpStatus.Success).json({ message: "Post request" });
   },
 
- async GET(req:NextApiRequest,res:NextApiResponse){
+  async GET(req: NextApiRequest, res: NextApiResponse) {
+    const { data, error } = await dbClient.from("newsletter_users").select("*");
 
-    const {data,error}= await dbClient.from("newsletter_users")
-    .select("*")
+    console.log("supabase data ==>> ", data);
+    console.log("supabase erros ==>> ", error);
 
-    console.log('supabase data ==>> ',data )
-    console.log('supabase erros ==>> ',error )
-
-    res
-    .status(httpStatus.Success)
-    .json({
-      message:'Get request',
-      data:data
-    })
+    res.status(httpStatus.Success).json({
+      message: "Get request",
+      data: data,
+    });
   },
-
-}
+};
 
 export default function handler(
-    request:NextApiRequest,
-  response:NextApiResponse
-){
- const controller= controllerByMethod[request.method];
- if(!controller){
-  response
-  .status(httpStatus.NotFound) 
-  .json({message:'Nada encontrado aqui :('})
-  return;
- } 
- 
- controller(request,response);
- 
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const controller = controllerByMethod[request.method];
+  if (!controller) {
+    response
+      .status(httpStatus.NotFound)
+      .json({ message: "Nada encontrado aqui :(" });
+    return;
+  }
+
+  controller(request, response);
 }
 
 // export default function handler(
@@ -85,4 +86,3 @@ export default function handler(
 
 //   const responseBody = {name:'Mario Souto'}
 //   response.status(httpStatus.Success).json({responseBody})
- 
